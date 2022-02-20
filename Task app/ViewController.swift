d4dda468cad786d4498ec9f1121f7a8e794b1758
RAM111111 <r.r.99@windowslive.com>
2022-02-20 15:21:05 +0300

    Initial Commit

---
+++ Task app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task app
+//
+//  Created by R on 19/07/1443 AH.
+//  Copyright © 1443 R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
